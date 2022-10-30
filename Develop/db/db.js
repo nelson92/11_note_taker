99b945894f8ac512b19eb6d76bdc8b8d1131e501
nelson509 <35314794+nelson509@users.noreply.github.com>
2022-10-29 18:41:18 -0700

    functions for db.js

--- Develop/db/db.js
+++ Develop/db/db.js
@@ -7,6 +7,17 @@ const uuid = require('uuid');
 
 
 
+app.use(express.static('public'));
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+//   double check the path for the 2nd parameter
+);
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+//   double check the path for the 2nd parameter 
+);
 
 // javascript class for db actions like read and write z
 // path: 'db/db.json'
